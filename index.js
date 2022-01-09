bbb3349db224e5a04173a0db4d89d7cd65bb03e4
Alan He <qianghe421@gmail.com>
2022-01-09 23:17:17 +0800

    测试md5加密

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const {KEYUTIL} = require('jsrsasign');
+const crypto = require('crypto');
 const fs = require('fs');
 const privateKey = fs.readFileSync('./alanhe.sign.key', 'utf8');
 const privateKeyObj = KEYUTIL.getKey(privateKey);
 console.log(privateKeyObj.pubKeyHex);
+
+const md5Msg = crypto.createHash('md5').update('123').digest("hex");
+console.log(md5Msg);
+
 
